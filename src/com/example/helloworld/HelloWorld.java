68f60d2ff5b76881b326bddd54a16b4c1debbc55
AQUIN0S <aquinos@acyonix.com>
2017-05-19 13:56:22 +1200

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+/**
+ * Created by danny on 19/05/17.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!!!");
+
+    }
+
+}
